59c4e10bced0564b791ef19f3a7edc3791114667
Francisco Lourenço Heleno <francisco.l.heleno@tecnico.ulisboa.pt>
2024-04-18 18:53:53 +0100

    Class Board implemented (hopefully ahah)

--- pipe.py
+++ pipe.py
@@ -15,28 +15,54 @@ class PipeManiaState:
 
 class Board:
     """ Representação interna de uma grelha de PipeMania. """
-    def __init__(self,grid):
+    def __init__(self, grid):
         self.grid = grid
-        pass
+        self.n = len(grid)
 
  
     def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
         """ Devolve os valores imediatamente acima e abaixo,
         respectivamente. """
-        return (self.grid[row - 1][col], self.grid[row + 1][col])
-        #falta verificações
-        
-        pass
+        if row not in (0, self.n - 1):
+            return (self.grid[row - 1][col], self.grid[row + 1][col])
+        elif row == 0:
+            return (None, self.grid[row + 1][col])
+        elif row == self.n - 1:
+            return (self.grid[row - 1][col], None)
+        else:
+            return (None, None)
+
 
     def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
         """ Devolve os valores imediatamente à esquerda e à direita,
         respectivamente. """
-        return (self.grid[row][col - 1], self.grid[row][col + 1])
-        #falta verificações
+        if col not in (0, self.n - 1):
+            return (self.grid[row][col - 1], self.grid[row][col + 1])
+        elif col == 0:
+            return (None, self.grid[row][col + 1])
+        elif col == self.n - 1:
+            return (self.grid[row][col - 1], None)
+        else:
+            return (None, None)
 
-        pass
     
-    # TODO: outros metodos da classe
+    def get_value(self, row: int, col: int) -> (str):
+        """ Devolve o valor preenchido na posição especificada."""
+        if 0 <= row <= self.n - 1 and 0 <= col <= self.n - 1:
+            return self.grid[row][col]
+        else:
+            return None
+
+    def __str__(self):
+        string = ""
+        for i in range(0, self.n):
+            for j in range(0, self.n):
+                if j == self.n - 1:
+                    string += self.grid[i][j] + "\n"
+                else:
+                    string += self.grid[i][j] + " " #verificar se é espaço ou tabulação
+        return string
+
 
     @staticmethod
     def parse_instance():
@@ -49,16 +75,22 @@ class Board:
             > from sys import stdin
             > line = stdin.readline().split()
         """
-        # TODO
         grid = []
 
         line = stdin.readline().split()
         while len(line) != 0:
             grid.append(line)
             line = stdin.readline().split()
-        
-        Board(grid)
-        return grid
 
+        #tabuleiro = Board(grid)
+        #print(tabuleiro)
+
+        return Board(grid)
+
+# Exemplo 1:
+# Ler grelha do figura 1a:
 board = Board.parse_instance()
-print(board.grid)
+print(board.adjacent_vertical_values(0, 0))
+print(board.adjacent_horizontal_values(0, 0))
+print(board.adjacent_vertical_values(1, 1))
+print(board.adjacent_horizontal_values(1, 1))
