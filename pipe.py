d6a30bd61ed14e5dabfe8126fe649bcfc63eef9f
Francisco Lourenço Heleno <francisco.l.heleno@tecnico.ulisboa.pt>
2024-04-24 12:14:17 +0100

    Defined rotations and some extra functions for the classes Board and PipeMania

--- pipe.py
+++ pipe.py
@@ -46,6 +46,16 @@ class Board:
             return self.grid[row][col]
         else:
             return None
+    
+    def get_type(self, row: int, col: int) -> str:
+        """Devolve o tipo da peça na respetiva posição do tabuleiro."""
+        piece_type = self.get_value(row, col)
+        return piece_type[0] if piece_type is not None else None
+    
+    def get_orientation(self, row: int, col: int) -> str:
+        """Devolve a orientação da peça na respetiva posição do tabuleiro."""
+        orientation = self.get_value(row, col)
+        return orientation[1] if orientation is not None else None
 
     def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
         """Devolve os valores imediatamente acima e abaixo,
@@ -76,6 +86,36 @@ class Board:
             return (None, None)
         """
         return (self.get_value(row, col - 1), self.get_value(row, col + 1))
+    
+    def set_value(self, row: int, col: int, clockwise: bool):
+        """Devolve uma nova instância da classe Board com a
+        alteração aplicada"""
+        all_orientations = ('C', 'D', 'B', 'E')
+        dic = {'H': 'V', 'V': 'H'}
+
+        #print("Value: " + self.get_value(row, col))
+
+        piece_type = self.get_type(row, col)
+        orientation = self.get_orientation(row, col)
+        current_orientation = all_orientations.index(orientation)
+        new_value = ''
+
+        if piece_type in ('F', 'B', 'V'):
+            if clockwise:
+                new_value += piece_type + all_orientations[(current_orientation + 1) % 4]
+            else:
+                new_value += piece_type + all_orientations[(current_orientation - 1) % 4]
+        else:
+            new_value += piece_type + dic[orientation]
+        
+        new_grid = self.grid
+        new_grid[row][col] = new_value
+
+        #print(self.get_value(row, col))
+        #print("new: " + new_value)
+
+        return Board(new_grid)
+
 
     @staticmethod
     def parse_instance():
@@ -99,6 +139,16 @@ class Board:
         # print(tabuleiro)
 
         return Board(grid)
+    
+    def print(self):
+        string = ""
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                if j == self.size - 1:
+                    string += self.grid[i][j] + "\n"
+                else:
+                    string += self.grid[i][j] + " " #verificar se é espaço ou tabulação
+        return string
 
     # TODO: outros metodos da classe
 
@@ -106,7 +156,8 @@ class Board:
 class PipeMania(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
-        # TODO
+        initial_state = PipeManiaState(board)
+        super().__init__(initial_state)
         pass
 
     def actions(self, state: PipeManiaState):
@@ -120,8 +171,8 @@ class PipeMania(Problem):
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        # TODO
-        pass
+        (row, col, direction) = action
+        return PipeManiaState(state.board.set_value(row, col, direction))
 
     def goal_test(self, state: PipeManiaState):
         """Retorna True se e só se o estado passado como argumento é
@@ -144,8 +195,52 @@ if __name__ == "__main__":
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
+
+    """
+    #Exemplo 1
     board = Board.parse_instance()
     print(board.adjacent_vertical_values(0, 0))
     print(board.adjacent_horizontal_values(0, 0))
     print(board.adjacent_vertical_values(1, 1))
     print(board.adjacent_horizontal_values(1, 1))
+    #print(board.get_type(0, 1))
+    #print(board.get_orientation(0, 1))
+    """
+
+    """
+    #Exemplo 2
+    # Ler grelha do figura 1a:
+    board = Board.parse_instance()
+    # Criar uma instância de PipeMania:
+    problem = PipeMania(board)
+    # Criar um estado com a configuração inicial:
+    initial_state = PipeManiaState(board)
+    # Mostrar valor na posição (2, 2):
+    print(initial_state.board.get_value(2, 2))
+    # Realizar ação de rodar 90° clockwise a peça (2, 2)
+    result_state = problem.result(initial_state, (2, 2, True))
+    # Mostrar valor na posição (2, 2):
+    print(result_state.board.get_value(2, 2))
+    """
+
+    #Exemplo 3
+    # Ler grelha do figura 1a:
+    board = Board.parse_instance()
+    # Criar uma instância de PipeMania:
+    problem = PipeMania(board)
+    # Criar um estado com a configuração inicial:
+    s0 = PipeManiaState(board)
+    # Aplicar as ações que resolvem a instância
+    s1 = problem.result(s0, (0, 1, True))
+    s2 = problem.result(s1, (0, 1, True))
+    s3 = problem.result(s2, (0, 2, True))
+    s4 = problem.result(s3, (0, 2, True))
+    s5 = problem.result(s4, (1, 0, True))
+    s6 = problem.result(s5, (1, 1, True))
+    s7 = problem.result(s6, (2, 0, False)) # anti-clockwise (exemplo de uso)
+    s8 = problem.result(s7, (2, 0, False)) # anti-clockwise (exemplo de uso)
+    s9 = problem.result(s8, (2, 1, True))
+    s10 = problem.result(s9, (2, 1, True))
+    s11 = problem.result(s10, (2, 2, True))
+
+    print("Solution:\n", s11.board.print(), sep="")
